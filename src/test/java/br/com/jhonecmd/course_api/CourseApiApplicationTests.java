4e13eaa0ab384af6762fbcfeb857afd558f8f78d
Jhone-cmd <jhone.1941@gmail.com>
2026-02-24 09:22:19 -0300

    intial commit

---
+++ src/test/java/br/com/jhonecmd/course_api/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jhonecmd.course_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
