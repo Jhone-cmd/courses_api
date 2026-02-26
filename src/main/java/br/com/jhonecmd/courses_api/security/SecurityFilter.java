90219056fad829c50f5c459bb9872ad86631dc5d
Jhone-cmd <jhone.1941@gmail.com>
2026-02-26 11:54:12 -0300

    creating filter for authorization.

---
+++ src/main/java/br/com/jhonecmd/courses_api/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package br.com.jhonecmd.courses_api.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import br.com.jhonecmd.courses_api.providers.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null) {
+            var token = this.jwtProvider.validateToken(header);
+
+            if (token == null) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+
+            request.setAttribute("userId", token.getSubject());
+
+            var roles = token.getClaim("roles").asList(Object.class);
+
+            var grants = roles.stream()
+                    .map((role) -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())).toList();
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null,
+                    grants);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
