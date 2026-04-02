9e3d7ec2d7daa0b104c2ece1379752bd5d5f5ba4
Jhone-cmd <jhone.1941@gmail.com>
2026-04-02 15:48:07 -0300

    tests to view all categories.

---
+++ src/test/java/br/com/jhonecmd/course_api/modules/category/usecases/FetchAllCategoryUseCaseTest.java
@@ -0,0 +1,53 @@
+package br.com.jhonecmd.course_api.modules.category.usecases;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.jhonecmd.courses_api.modules.category.entities.CategoryEntity;
+import br.com.jhonecmd.courses_api.modules.category.repositories.CategoryRepository;
+import br.com.jhonecmd.courses_api.modules.category.usecases.FetchAllCategoryUseCase;
+
+@ExtendWith(MockitoExtension.class)
+public class FetchAllCategoryUseCaseTest {
+
+    @InjectMocks
+    private FetchAllCategoryUseCase fetchAllCategoryUseCase;
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("Should be able to list all categories.")
+    public void should_be_able_to_list_all_user() {
+
+        var category1 = CategoryEntity.builder()
+                .id(UUID.randomUUID())
+                .name("Tecnologia")
+                .build();
+
+        var category2 = CategoryEntity.builder()
+                .id(UUID.randomUUID())
+                .name("Administração")
+                .build();
+
+        when(categoryRepository.findAll()).thenReturn(List.of(category1, category2));
+
+        var result = fetchAllCategoryUseCase.execute();
+
+        assertThat(result).hasSize(2);
+        verify(categoryRepository, times(1)).findAll();
+    }
+}
