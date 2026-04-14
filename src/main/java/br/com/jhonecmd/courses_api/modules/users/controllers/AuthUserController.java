28da43b8461c4271c7627d82618f3becfb8f3044
Jhone-cmd <jhone.1941@gmail.com>
2026-04-14 15:42:21 -0300

    adjusting user routes in Swagger UI.

--- src/main/java/br/com/jhonecmd/courses_api/modules/users/controllers/AuthUserController.java
+++ src/main/java/br/com/jhonecmd/courses_api/modules/users/controllers/AuthUserController.java
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jhonecmd.courses_api.modules.users.dto.AuthUserDTO;
+import br.com.jhonecmd.courses_api.modules.users.dto.AuthUserResponseDTO;
 import br.com.jhonecmd.courses_api.modules.users.usecases.AuthenticateUserUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +28,16 @@ public class AuthUserController {
     private AuthenticateUserUseCase authenticateUserUseCase;
 
     @PostMapping("/auth")
+    @Operation(summary = "User authentication.", description = "This route is designed to user authentication.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Token generated successfully.", content = {
+                    @Content(schema = @Schema(implementation = AuthUserResponseDTO.class))
+            }),
+
+            @ApiResponse(responseCode = "401", description = "Invalid Credentials", content = {
+                    @Content(schema = @Schema(implementation = String.class, example = "Invalid Credentials."))
+            })
+    })
     public ResponseEntity<Object> session(@RequestBody AuthUserDTO authUserDTO) {
         try {
 
--- src/main/java/br/com/jhonecmd/courses_api/modules/users/controllers/UserController.java
+++ src/main/java/br/com/jhonecmd/courses_api/modules/users/controllers/UserController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.jhonecmd.courses_api.exceptions.ErrorMessageDTO;
 import br.com.jhonecmd.courses_api.exceptions.UserAlreadyExists;
 import br.com.jhonecmd.courses_api.modules.users.dto.CreateUserDTO;
+import br.com.jhonecmd.courses_api.modules.users.dto.UserResponseDTO;
 import br.com.jhonecmd.courses_api.modules.users.entities.UserEntity;
 import br.com.jhonecmd.courses_api.modules.users.usecases.CreateUserUseCase;
 import br.com.jhonecmd.courses_api.modules.users.usecases.FetchAllUserUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -30,6 +38,16 @@ public class UserController {
     private FetchAllUserUseCase fetchUserUseCase;
 
     @PostMapping("")
+    @Operation(summary = "Create a user.", description = "This route is designed to create a user.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+
+            @ApiResponse(responseCode = "409", description = "User already exists.", content = {
+                    @Content(schema = @Schema(implementation = UserAlreadyExists.class, example = "User already exists!"))
+            }),
+
+            @ApiResponse(responseCode = "400", description = "Validation errors", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ErrorMessageDTO.class))))
+    })
     public ResponseEntity<Object> create(@Valid @RequestBody CreateUserDTO createUserDTO) {
         try {
 
@@ -48,6 +66,10 @@ public class UserController {
     }
 
     @GetMapping("")
+    @Operation(summary = "View users.", description = "This route is designed to view users.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List of users retrieved successfully", content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserResponseDTO.class))))
+    })
     public ResponseEntity<Object> fetchAllUsers() {
         try {
 
