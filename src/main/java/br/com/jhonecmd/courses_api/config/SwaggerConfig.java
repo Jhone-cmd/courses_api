99e42b7f42e5601897cb8e288fefac159effd6e6
Jhone-cmd <jhone.1941@gmail.com>
2026-04-14 15:13:48 -0300

    installing and configuring Swagger UI.

---
+++ src/main/java/br/com/jhonecmd/courses_api/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package br.com.jhonecmd.courses_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Courses API")
+                        .description(
+                                "This API allows for complete management of a course catalog. Through it, you can register new training courses, toggle their availability (active), and perform refined searches by name or category. Ideal for LMS (Learning Management Systems).")
+                        .version("1.0"));
+    }
+}
